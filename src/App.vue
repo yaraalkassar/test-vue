55ac6e3b20d3e26e2d6439895b725cc7c256d262
Yara <yara.sarmad@yahoo.com>
2022-09-18 22:20:50 +0300

    done

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ul>
+    <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }">
+      <span @click="selectTab(tab.name)">{{ tab.name }}</span>
+    </li>
+  </ul>
+  <div id="text">
+    <p v-if="activeTab == 'Info'">
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the
+      1500s, when an unknown printer took a galley of type and scrambled it to
+      make a type specimen book. It has survived not only five centuries, but
+      also the leap into electronic typesetting, remaining essentially
+      unchanged. It was popularised in the 1960s with the release of Letraset
+      sheets containing Lorem Ipsum passages, and more recently with desktop
+      publishing software like Aldus PageMaker including versions of Lorem
+      Ipsum.
+    </p>
+    <p v-else-if="activeTab == 'Phone'">I AM PHONE INFO</p>
+    <p v-else-if="activeTab == 'Email'">I AM Email INFO</p>
+    <p v-else-if="activeTab == 'GitHub'">I AM GitHub INFO</p>
+    <p v-else-if="activeTab == 'LinkedIn'">I AM LinkedIn INFO</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      tabs: [
+        { name: "Info", isActive: true },
+        { name: "Phone", isActive: false },
+        { name: "Email", isActive: false },
+        { name: "GitHub", isActive: false },
+        { name: "LinkedIn", isActive: false },
+      ],
+      activeTab: "Info",
+    };
+  },
+  components: {},
+  methods: {
+    selectTab(selectedTab) {
+      this.activeTab = selectedTab;
+
+      this.tabs.forEach((tab) => {
+        tab.isActive = tab.name == selectedTab;
+      });
+    },
+  },
+};
 </script>
 
 <style>
@@ -22,5 +59,31 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+ul {
+  border-bottom: 2px solid gray;
+  display: flex;
+  justify-content: space-between;
+  width: 50%;
+  margin: auto;
+  padding: 10px 0;
+}
+#text {
+  width: 50%;
+  margin: auto;
+  padding: 30px 0;
+}
+
+li {
+  list-style: none;
+  padding: 0 40px;
+  border-right: 2px solid gray;
+  cursor: pointer;
+}
+ul :last-child {
+  border: none;
+}
+.is-active {
+  background-color: cadetblue;
 }
 </style>
